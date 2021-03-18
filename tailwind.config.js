3d5efa8c29222d218a7996512930e0565ccc50fb
Klára Neumannová <kl.neumannova@gmail.com>
2021-03-18 11:43:52 +0100

    Background fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      extend: {
-        backgroundImage: theme => ({
-          unsplash: "url('https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1955&q=80')",
-        }),
-      },
+      backgroundImage: theme => ({
+        unsplash: "url('https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1955&q=80')",
+      })
     },
   },
   variants: {
